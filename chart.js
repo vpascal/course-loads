7e2db3a3bfb61917fdaeaef54b8409f4177a14cd
vpascal <vpascal2@gmail.com>
2019-04-08 14:45:32 -0400

    updates
    data filtered
    new function created

--- chart.js
+++ chart.js
@@ -39,145 +39,174 @@ function wrap(text, width) {
 }        
 
 //Read the data
-d3.csv("data_small.csv", function(data) {
+function draw (dataset){
+  d3.csv(dataset, function(data) {
 
-  // Labels of row and columns -> unique identifier of the column called 'Term' and 'variable'
-  const myGroups = d3.map(data, function(d){return d.Term;}).keys()
-  const myVars = d3.map(data, function(d){return d.course;}).keys()
-  myVars.sort().reverse();
-
-  // Build X scales and axis:
-  const x = d3.scaleBand()
-    .range([ 0, width ])
-    .padding(0.1)
-    .domain(['Fall 13-14','Spr 13-14','Sum 13-14','Fall 14-15','Spr 14-15','Sum 14-15',
-                  'Fall 15-16','Spr 15-16','Sum 15-16',
-                  'Fall 16-17','Spr 16-17','Sum 16-17',
-                  'Fall 17-18','Spr 17-18','Sum 17-18']);
-            
+    // Labels of row and columns -> unique identifier of the column called 'Term' and 'variable'
+    const myGroups = d3.map(data, function(d){return d.Term;}).keys()
+    const myVars = d3.map(data, function(d){return d.course;}).keys()
+    myVars.sort().reverse();
   
- svg.append("g")
-    .style("font-size", 12)
-    .style('font-family','monospace')
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(0))
-    .attr('id','xtext')
-    .select(".domain").remove()
-
-svg.append("g")
-    .style("font-size", 12)
-    .style('font-family','monospace')
-    .attr("transform", "translate(0,-30)")
-    .call(d3.axisTop(x).tickSize(0))
-    .attr('id','xtext')
-    .select(".domain").remove()    
- 
-  // Build Y scales and axis:
-  const y = d3.scaleBand()
-    .range([ height, 0 ])
-    .domain(myVars)
-    .padding(0.1);
-
+    // Build X scales and axis:
+    const x = d3.scaleBand()
+      .range([ 0, width ])
+      .padding(0.1)
+      .domain(['Fall 13-14','Spr 13-14','Sum 13-14','Fall 14-15','Spr 14-15','Sum 14-15',
+                    'Fall 15-16','Spr 15-16','Sum 15-16',
+                    'Fall 16-17','Spr 16-17','Sum 16-17',
+                    'Fall 17-18','Spr 17-18','Sum 17-18']);
+              
+    
+   svg.append("g")
+      .style("font-size", 12)
+      .style('font-family','monospace')
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0))
+      .attr('id','xtext')
+      .select(".domain").remove()
+  
   svg.append("g")
-    .style("font-size", 12)
-    .style('font-family','monospace')
-    .call(d3.axisLeft(y).tickSize(0))
-    .select(".domain").remove()
-
-//dual y axis
-svg.append("g")
-    .style("font-size", 12)
-    .style('font-family','monospace')
-    .attr("transform", "translate( " + width + ", 0 )")
-    .call(d3.axisRight(y).tickSize(0))
-    .select(".domain").remove()
-
-
-  // Build color scale
-  let myColor = d3.scaleSequential()
-    .interpolator(d3.interpolateOranges)
-    .domain([1,100])
-
-  // create a tooltip
-  let tooltip = d3.select("#dataviz")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("font-family", "'Roboto', sans-serif")
-    .style("font-size", '14px')
-    .style('color','white')
-    .style("background-color", "#0282f9")
-    .style('box-shadow', '0 1px 8px rgba(0,0,0,0.5)')
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-
-  // Three function that change the tooltip when user hover / move / leave a cell
-  let mouseover = function(d) {
-
-    tooltip
-      .style("opacity", 1)
-
-    d3.select(this)
-      .style("stroke", "red")
-      .style("opacity", 1)
-
-          
-  }
-  let mousemove = function(d) {
-    tooltip
-      .html("Percent: " + d.percent 
-                + "<br>Term: "+ d.Term
-                + "<br>Course ID: "+d.course
-                + "<br>Course: "+d.class)
-      .style("left", (d3.mouse(this)[0]+10) + "px")
-      .style("top", (d3.mouse(this)[1]+100) + "px")
-  }
-  let mouseleave = function(d) {
-    tooltip
-      .style("opacity", 0)
-    d3.select(this)
-      .style("stroke", "none")
-      .style("opacity", 0.8) 
-  }
-
-  svg.append('defs')
-            .append('pattern')
-            .attr('id', 'diagonalHatch')
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('width', 4)
-            .attr('height', 4)
-            .append('path')
-            .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
-            .attr('stroke', '#919191')
-            .attr('stroke-width', 1);
-
-
-  // add the squares
-  svg.selectAll()
-    .data(data, function(d) {return d.Term+':'+d.course;})
-    .enter()
-    .append("rect")
-      .attr("x", function(d) { return x(d.Term) })
-      .attr("y", function(d) { return y(d.course) })
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { 
-                          if(d.percent !==''){
-                            return myColor(d.percent);
-                          } else { return "url(#diagonalHatch)";}
-                              } )
-      .style("stroke-width", 4)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
+      .style("font-size", 12)
+      .style('font-family','monospace')
+      .attr("transform", "translate(0,-30)")
+      .call(d3.axisTop(x).tickSize(0))
+      .attr('id','xtext')
+      .select(".domain").remove()    
+   
+    // Build Y scales and axis:
+    const y = d3.scaleBand()
+      .range([ height, 0 ])
+      .domain(myVars)
+      .padding(0.1);
   
+    svg.append("g")
+      .style("font-size", 12)
+      .style('font-family','monospace')
+      .call(d3.axisLeft(y).tickSize(0))
+      .select(".domain").remove()
+  
+  //dual y axis
+  svg.append("g")
+      .style("font-size", 12)
+      .style('font-family','monospace')
+      .attr("transform", "translate( " + width + ", 0 )")
+      .call(d3.axisRight(y).tickSize(0))
+      .select(".domain").remove()
+  
+  
+    // Build color scale
+    let myColor = d3.scaleSequential()
+      .interpolator(d3.interpolateOranges)
+      .domain([1,100])
+  
+    // create a tooltip
+    let tooltip = d3.select("#dataviz")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("font-family", "'Roboto', sans-serif")
+      .style("font-size", '14px')
+      .style('color','white')
+      .style("background-color", "#0282f9")
+      .style('box-shadow', '0 1px 8px rgba(0,0,0,0.5)')
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+  
+    // Three function that change the tooltip when user hover / move / leave a cell
+    let mouseover = function(d) {
+  
+      tooltip
+        .style("opacity", 1)
+  
+      d3.select(this)
+        .style("stroke", "red")
+        .style("opacity", 1)
+  
+            
+    }
+    let mousemove = function(d) {
+      tooltip
+        .html("Percent: " + d.percent 
+                  + "<br>Term: "+ d.Term
+                  + "<br>Course ID: "+d.course
+                  + "<br>Course: "+d.class)
+        .style("left", (d3.mouse(this)[0]+10) + "px")
+        .style("top", (d3.mouse(this)[1]+100) + "px")
+    }
+    let mouseleave = function(d) {
+      tooltip
+        .style("opacity", 0)
+      d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8) 
+    }
+  
+    svg.append('defs')
+              .append('pattern')
+              .attr('id', 'diagonalHatch')
+              .attr('patternUnits', 'userSpaceOnUse')
+              .attr('width', 4)
+              .attr('height', 4)
+              .append('path')
+              .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
+              .attr('stroke', '#919191')
+              .attr('stroke-width', 1);
+  
+  
+    // add the squares
+    svg.selectAll()
+      .data(data, function(d) {return d.Term+':'+d.course;})
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return x(d.Term) })
+        .attr("y", function(d) { return y(d.course) })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) { 
+                            if(d.percent !==''){
+                              return myColor(d.percent);
+                            } else { return "url(#diagonalHatch)";}
+                                } )
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+    
+  
+   
+  
+  svg.selectAll('#xtext text').call(wrap, 10); 
+  
+  })
+  
+}
 
- 
+//tracking the active button
+//const header = document.getElementById("department");
+//const btns = header.getElementsByClassName("btn");
 
-svg.selectAll('#xtext text').call(wrap, 10); 
+const btns =  d3.select('#department').selectAll('.btn');
 
-})
+btns.on('click',function(){
+
+  d3.select(this).attr('class','active');
+
+});
+
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  const button_value = current[0].name;
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+const radio_value = d3.select('input[name="level"]:checked').node().value;
+draw('data_small.csv');
+
