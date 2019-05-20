491c4345cf79170c4bd7ad077c92846a6d70c1d4
vpascal <vpascal2@gmail.com>
2019-05-20 13:34:08 -0400

    formating
    formatting the code

--- grid.js
+++ grid.js
@@ -1,4 +1,3 @@
-
 var margin_g = { top: 40, right: 20, bottom: 30, left: 20 },
     width_g = 250 - margin_g.left - margin_g.right,
     height_g = 250 - margin_g.top - margin_g.bottom;
@@ -8,21 +7,21 @@ var div = d3.select("body").append("div")
     .style("opacity", 0);
 
 //Read the data
-d3.csv("fillrate.csv", function (data) {
+d3.csv("fillrate.csv", function(data) {
 
-    var ugrad = data.filter(function (d) { return (d.level == 'Undergraduate'); })
-    var grad = data.filter(function (d) { return (d.level == 'Graduate'); })
+    var ugrad = data.filter(function(d) { return (d.level == 'Undergraduate'); })
+    var grad = data.filter(function(d) { return (d.level == 'Graduate'); })
 
     ugrad = d3.nest()
-        .key(function (d) { return d.Department; })
+        .key(function(d) { return d.Department; })
         .entries(ugrad);
 
     grad = d3.nest()
-        .key(function (d) { return d.Department; })
+        .key(function(d) { return d.Department; })
         .entries(grad);
 
 
-    allKeys = ugrad.map(function (d) { return d.key })
+    allKeys = ugrad.map(function(d) { return d.key })
 
     var svg_g = d3.select("#grid")
         .selectAll("uniqueChart")
@@ -50,13 +49,13 @@ d3.csv("fillrate.csv", function (data) {
     var y_g = d3.scaleLinear()
         .domain([0, 100])
         .range([height_g, 0]);
-  
+
 
     svg_g.append("g")
-        .call(d3.axisLeft(y_g).ticks(10))//.tickSize(-width_g)//
+        .call(d3.axisLeft(y_g).ticks(10)) //.tickSize(-width_g)//
         .call(g => g.select(".domain").remove())
         .attr("class", "axis");
-      //  .style("stroke-dasharray",("1,1"));
+    //  .style("stroke-dasharray",("1,1"));
 
     // Draw the line for undergrads
     svg_g
@@ -65,10 +64,10 @@ d3.csv("fillrate.csv", function (data) {
         .attr("stroke", '#03a9f4')
         .attr('id', 'ugrad')
         .attr("stroke-width", 1.9)
-        .attr("d", function (d) {
+        .attr("d", function(d) {
             return d3.line()
-                .x(function (d) { return x_g(d.year)+21; })
-                .y(function (d) { return y_g(+d.percent); })
+                .x(function(d) { return x_g(d.year) + 21; })
+                .y(function(d) { return y_g(+d.percent); })
                 .curve(d3.curveMonotoneX)
                 (d.values);
         });
@@ -76,40 +75,40 @@ d3.csv("fillrate.csv", function (data) {
     // undergrad  circles            
     svg_g
         .selectAll('.grid')
-        .data(function (d) { return d.values })
+        .data(function(d) { return d.values })
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return x_g(d.year)+21 })
-        .attr("cy", function (d) { return y_g(+d.percent) })
+        .attr("cx", function(d) { return x_g(d.year) + 21 })
+        .attr("cy", function(d) { return y_g(+d.percent) })
         .attr("r", 4)
         .attr('fill', '#03a9f4')
         .attr("stroke", "white")
-        .on("mouseover", function (d) {
+        .on("mouseover", function(d) {
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
             div.html(d.percent + '%')
-                .style("left", (d3.event.pageX)-25 + "px")
+                .style("left", (d3.event.pageX) - 25 + "px")
                 .style("top", (d3.event.pageY - 30) + "px");
         })
-        .on("mouseout", function (d) {
+        .on("mouseout", function(d) {
             div.transition()
                 .duration(500)
                 .style("opacity", 0);
         });
 
-// label
+    // label
     svg_g
         .selectAll('.grid')
-        .data(function (d) { return d.values })
+        .data(function(d) { return d.values })
         .enter()
         .append("text")
-            .datum('Undergraduate')
-            .attr("transform", 'translate(17,10)')
-            .text('Undergraduate')
-            .style("fill",'#03a9f4' )
-            .style("font-size", 11)
-            .style('font-family', 'monospace');
+        .datum('Undergraduate')
+        .attr("transform", 'translate(17,10)')
+        .text('Undergraduate')
+        .style("fill", '#03a9f4')
+        .style("font-size", 11)
+        .style('font-family', 'monospace');
 
 
 
@@ -121,10 +120,10 @@ d3.csv("fillrate.csv", function (data) {
         .attr("fill", "none")
         .attr("stroke", '#8bc34a')
         .attr("stroke-width", 1.9)
-        .attr("d", function (d) {
+        .attr("d", function(d) {
             return d3.line()
-                .x(function (d) { return x_g(d.year)+21; })
-                .y(function (d) { return y_g(+d.percent); })
+                .x(function(d) { return x_g(d.year) + 21; })
+                .y(function(d) { return y_g(+d.percent); })
                 .curve(d3.curveMonotoneX)
                 (d.values)
         });
@@ -132,39 +131,39 @@ d3.csv("fillrate.csv", function (data) {
     // grad  circles            
     svg_g
         .selectAll('.grid')
-        .data(function (d) { return d.values })
+        .data(function(d) { return d.values })
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return x_g(d.year)+21 })
-        .attr("cy", function (d) { return y_g(+d.percent) })
+        .attr("cx", function(d) { return x_g(d.year) + 21 })
+        .attr("cy", function(d) { return y_g(+d.percent) })
         .attr("r", 4)
         .attr('fill', '#8bc34a')
         .attr("stroke", "white")
-        .on("mouseover", function (d) {
+        .on("mouseover", function(d) {
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
             div.html(d.percent + "%")
-                .style("left", (d3.event.pageX)-25 + "px")
+                .style("left", (d3.event.pageX) - 25 + "px")
                 .style("top", (d3.event.pageY - 30) + "px");
         })
-        .on("mouseout", function (d) {
+        .on("mouseout", function(d) {
             div.transition()
                 .duration(500)
                 .style("opacity", 0);
         });
 
-     svg_g
+    svg_g
         .selectAll('.grid')
-        .data(function (d) { return d.values })
+        .data(function(d) { return d.values })
         .enter()
         .append("text")
-            .datum('Grad')
-            .attr("transform", 'translate(17,22)')
-            .text('Graduate')
-            .style("fill",'#8bc34a' )
-            .style("font-size", 11)
-            .style('font-family', 'monospace');
+        .datum('Grad')
+        .attr("transform", 'translate(17,22)')
+        .text('Graduate')
+        .style("fill", '#8bc34a')
+        .style("font-size", 11)
+        .style('font-family', 'monospace');
 
     // Add titles
     svg_g
@@ -172,7 +171,7 @@ d3.csv("fillrate.csv", function (data) {
         .attr("text-anchor", "start")
         .attr("y", -7)
         .attr("x", 0)
-        .text(function (d) { return (d.key) })
+        .text(function(d) { return (d.key) })
         .style("fill", '#676767')
         .style("font-size", 16)
         .style('font-family', 'monospace')
